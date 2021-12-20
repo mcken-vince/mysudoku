7baa52c10ed4a14c12af9e7a3a42795b13b81647
mcken-vince <mcken.vince@gmail.com>
2021-12-20 11:28:32 -0700

    Use classNames to give right-diagonal or left-diagonal class to squares that are on the diagonal lines

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -14,10 +14,25 @@ const Square = (props: SquareProps) => {
     }
   };
 
+  let isLeftDiagonal = false;
+  let isRightDiagonal = false;
+  if (square.col === square.row) isLeftDiagonal = true;
+  if (square.col + square.row === 8) isRightDiagonal = true;
   const isSelectedSquare: boolean = selectedSquare ? ((selectedSquare.col === square.col) && (selectedSquare.row === square.row)) : false;
   const isSameValueAsSelectedSquare: boolean = (selectedSquare && square.value !== 0) ? (selectedSquare.value === square.value) : false;
   const sameRowColSquare: boolean = (selectedSquare && !isSelectedSquare) ? (selectedSquare.row === square.row || selectedSquare.col === square.col || selectedSquare.square === square.square) : false;
-  const squareClasses: string = classNames('square', 'noselect', { changeable: square.changeable, zero: (square.value === 0), selected: isSelectedSquare, highlight: isSameValueAsSelectedSquare, sameRowColSquare });
+  
+  const squareClasses: string = classNames(
+    'square', 
+    'noselect', 
+    { 
+      changeable: square.changeable, zero: (square.value === 0), 
+      selected: isSelectedSquare, 
+      highlight: isSameValueAsSelectedSquare, sameRowColSquare,
+      'left-diagonal': isLeftDiagonal,
+      'right-diagonal': isRightDiagonal
+    }
+  );
   
   return (
     <div onClick={handleSquareClick} className={squareClasses}>         
