db18551ea88303a1f1fce465fb7077bd31a8c72a
mcken-vince <mcken.vince@gmail.com>
2021-12-21 17:59:42 -0700

    Write out some descriptions and use Popover in SelectSudokuMode component

--- src/components/SelectSudokuMode.tsx
+++ src/components/SelectSudokuMode.tsx
@@ -2,20 +2,44 @@ import Button from 'react-bootstrap/Button';
 import '../styles/SelectSudokuMode.scss';
 import { ModeType } from '../logic/Main';
 import { useState } from 'react';
+import { Popover } from 'react-bootstrap';
 
 const SelectSudokuMode = (props: SelectSudokuModeProps) => {
   const [selectedMode, setSelectedMode] = useState<ModeType | null>(null);
   const onConfirm = props.onConfirm;
 
-  const classicMode = {
+  const classicDescription = {
     title: 'Classic Sudoku',
     description: "Regular 9x9 sudoku. Each row, each column, and each 9x9 area must have the numbers 1-9, no duplicates allowed."
   };
-
-  const diagonalMode = {
+  const diagonalDescription = {
     title: 'Diagonal Sudoku',
     description: "Just like classic sudoku, except now both diagonals have the numbers 1-9 also, no duplicates."
   };
+  const oddDescription = {
+    title: 'Odd Sudoku',
+    description: "Just like classic sudoku, but the highlighted squares must be odd numbers."
+  };
+  const evenDescription = {
+    title: 'Even Sudoku',
+    description: "Just like classic sudoku, but the highlighted squares must be even numbers."
+  };
+  const sumDescription = {
+    title: 'Sum Sudoku',
+    description: ""
+  };
+  const multiDescription = {
+    title: 'Multi Sudoku',
+    description: ""
+  };
+
+  let displayDescription;
+  if (selectedMode === 'multi') displayDescription = multiDescription;
+  else if (selectedMode === 'diagonal') displayDescription = diagonalDescription;
+  else if (selectedMode === 'odd') displayDescription = oddDescription;
+  else if (selectedMode === 'even') displayDescription = evenDescription;
+  else if (selectedMode === 'sum') displayDescription = sumDescription;
+  else displayDescription = classicDescription;
 
   return (
     <div className='select-mode-container'>
@@ -41,7 +65,12 @@ const SelectSudokuMode = (props: SelectSudokuModeProps) => {
       <Button variant={selectedMode === 'even' ? 'secondary' : 'primary'}disabled className='even-button' onClick={() => setSelectedMode('even')}>Even</Button>
       <Button variant={selectedMode === 'sum' ? 'secondary' : 'primary'}disabled className='sum-button' onClick={() => setSelectedMode('sum')}>Sum</Button>
       <Button variant={selectedMode === 'multi' ? 'secondary' : 'primary'} disabled className='multi-button' onClick={() => setSelectedMode('multi')}>Multi</Button>
-      <Button variant='success' onClick={() => onConfirm(selectedMode)}>Confirm</Button>
+      <Button className='confirm-button' variant='success' onClick={() => onConfirm(selectedMode)}>Confirm</Button>
+
+      <Popover placement='bottom'  id='popover' show={selectedMode && displayDescription ? true : false}>
+      <Popover.Header>{displayDescription && displayDescription.title}</Popover.Header>
+      <Popover.Body>{displayDescription && displayDescription.description}</Popover.Body>
+    </Popover>
     </div>
   );
 };
