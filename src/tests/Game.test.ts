c90846ab28291c1d352306d113d265790b0597aa
mcken-vince <mcken.vince@gmail.com>
2021-12-29 15:22:06 -0700

    Write unit test for shuffleRow()

--- src/tests/Game.test.ts
+++ src/tests/Game.test.ts
@@ -1,6 +1,6 @@
-import { generateBoard } from '../logic/Game';
+import { generateBoard, shuffleRow } from '../logic/Game';
 
-describe('generateBoard', () => {
+describe('generateBoard()', () => {
   it("generates a new board when calling generateBoard('easy', 'classic')", async () => {
     const result = await generateBoard('easy', 'classic');
     // Should return new puzzle and solution for puzzle
@@ -12,4 +12,23 @@ describe('generateBoard', () => {
     expect(newBoard[0].length).toBe(9)
   });
 
+
+});
+
+describe('shuffleRow()', () => {
+  it('returns a row array in a different order than the one inputted', () => {
+    const row = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    shuffleRow(row);
+    // Counts how many elements in the array are in a different position
+    let differences = 0;
+    let count = 1;
+    row.forEach(number => {
+      if (number !== count) {
+        differences++;
+      }
+      count++;
+    });
+
+    expect(differences).toBeGreaterThan(0);
+  })
 });
