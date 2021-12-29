be8206cba5ea918574403db566bcb9a15785429e
mcken-vince <mcken.vince@gmail.com>
2021-12-29 15:12:07 -0700

    Write passing test for generateBoard()

--- src/logic/Game.test.ts
+++ src/logic/Game.test.ts
@@ -1,9 +1,15 @@
-// import { generateBoard } from './Game';
+import { generateBoard } from './Game';
 
 describe('generateBoard', () => {
-  it.skip('generates a new board when calling method generateBoard', () => {
-    // const result = generateBoard();
-    // expect(result.length).toBe(81);
+  it("generates a new board when calling generateBoard('easy', 'classic')", async () => {
+    const result = await generateBoard('easy', 'classic');
+    // Should return new puzzle and solution for puzzle
+    expect(result.length).toBe(2);
+    // Puzzle should have 9 row arrays
+    const newBoard = result[0];
+    expect(newBoard.length).toBe(9);
+    // Rows in puzzle should have 9 elements
+    expect(newBoard[0].length).toBe(9)
   });
 
 });
