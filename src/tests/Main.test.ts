2fcd15c8b55b8da15ec96af62d763c6f37f42d49
mcken-vince <mcken.vince@gmail.com>
2021-12-30 15:20:05 -0700

    Write unit tests for checkNumberGrid

--- src/tests/Main.test.ts
+++ src/tests/Main.test.ts
@@ -1,7 +1,15 @@
-import {  } from '../logic/Main';
+import { checkNumberGrid } from '../logic/Main';
 
-describe(':)', () => {
-  it('', () => {
+describe('checkNumberGrid()', () => {
+  it("returns false if grid contains any 0's", () => {
+    const incompleteGrid = [[1,2,3,4,5,6,7,0 ],[0,0,0,0,0,0,0,0,0],[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],[0,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],[0,0,0,0,3,4,5,6],[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8]];
+    const result = checkNumberGrid(incompleteGrid);
+    expect(result).toBe(false);
+  });
 
+  it("returns true if grid contains no 0's", () => {
+    const completeGrid = [[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8]];
+    const result = checkNumberGrid(completeGrid);
+    expect(result).toBe(true);
   });
 });
