c1412d046c5ad3ac2dc399ce2594b9d409923d20
mcken-vince <mcken.vince@gmail.com>
2021-12-30 15:43:11 -0700

    Write second unit test for diagonalCheck();

--- src/tests/Main.test.ts
+++ src/tests/Main.test.ts
@@ -146,7 +146,7 @@ const numberGridNoConflicts: NumberGridType =
     [4,5,2,3,8,1,6,9,7],
     [3,6,9,5,4,7,8,2,1],
     [7,1,8,2,9,6,5,4,3],
-    [2,8,4,7,6,3,9,1,5],
+    [2,8,4,0,6,3,9,1,5],
     [5,9,3,4,1,8,7,6,2],
     [6,7,1,9,5,2,4,3,8],
     [9,2,6,1,7,5,3,8,4],
@@ -183,6 +183,11 @@ describe('checkGrid()', () => {
 describe('diagonalCheck()', () => {
   it('returns true if square is not in either diagonal', () => {
     const result = diagonalCheck(numberGridNoConflicts, 1, 5, 8);
+    expect(result).toBe(true);
+  });
+
+  it('returns true if square is on diagonal and the value given is not in the diagonal', () => {
+    const result = diagonalCheck(numberGridNoConflicts, 3, 3, 7);
     expect(result).toBe(true);
   });
 });
