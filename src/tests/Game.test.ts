ab95487093d616a7e0b931dcaee4358ac798a65e
mcken-vince <mcken.vince@gmail.com>
2022-01-07 11:41:45 -0700

    Update generateBoard test to reflect latest changes, all tests passing

--- src/tests/Game.test.ts
+++ src/tests/Game.test.ts
@@ -1,62 +1,60 @@
 import { generateBoard, GridType, makeCopyOfGrid, randomIndex, rowsToRegions, secondsToTimeString, shuffleRow } from '../logic/Game';
 
 const grid: GridType = [
-  [ {row: 0, col: 0, value: 1, square: 0},{row: 0, col: 1, value: 2, square: 0},{row: 0, col: 2, value: 3, square: 0},
-    {row: 0, col: 3, value: 4, square: 1},{row: 0, col: 4, value: 5, square: 1},{row: 0, col: 5, value: 6, square: 1},
-    {row: 0, col: 6, value: 7, square: 2},{row: 0, col: 7, value: 8, square: 2},{row: 0, col: 8, value: 9, square: 2}
+  [ {row: 0, col: 0, value: 1, square: 0, solution: 1},{row: 0, col: 1, value: 2, square: 0, solution: 1},{row: 0, col: 2, value: 3, square: 0, solution: 1},
+    {row: 0, col: 3, value: 4, square: 1, solution: 1},{row: 0, col: 4, value: 5, square: 1, solution: 1},{row: 0, col: 5, value: 6, square: 1, solution: 1},
+    {row: 0, col: 6, value: 7, square: 2, solution: 1},{row: 0, col: 7, value: 8, square: 2, solution: 1},{row: 0, col: 8, value: 9, square: 2, solution: 1}
   ],
   [
-    {row: 1, col: 0, value: 1, square: 0},{row: 1, col: 1, value: 2, square: 0},{row: 1, col: 2, value: 3, square: 0},
-    {row: 1, col: 3, value: 4, square: 1},{row: 1, col: 4, value: 5, square: 1},{row: 1, col: 5, value: 6, square: 1},
-    {row: 1, col: 6, value: 7, square: 2},{row: 1, col: 7, value: 8, square: 2},{row: 1, col: 8, value: 9, square: 2}
+    {row: 1, col: 0, value: 1, square: 0, solution: 1},{row: 1, col: 1, value: 2, square: 0, solution: 1},{row: 1, col: 2, value: 3, square: 0, solution: 1},
+    {row: 1, col: 3, value: 4, square: 1, solution: 1},{row: 1, col: 4, value: 5, square: 1, solution: 1},{row: 1, col: 5, value: 6, square: 1, solution: 1},
+    {row: 1, col: 6, value: 7, square: 2, solution: 1},{row: 1, col: 7, value: 8, square: 2, solution: 1},{row: 1, col: 8, value: 9, square: 2, solution: 1}
   ],
   [
-    {row: 2, col: 0, value: 1, square: 0},{row: 2, col: 1, value: 2, square: 0},{row: 2, col: 2, value: 3, square: 0},
-    {row: 2, col: 3, value: 4, square: 1},{row: 2, col: 4, value: 5, square: 1},{row: 2, col: 5, value: 6, square: 1},
-    {row: 2, col: 6, value: 7, square: 2},{row: 2, col: 7, value: 8, square: 2},{row: 2, col: 8, value: 9, square: 2}
+    {row: 2, col: 0, value: 1, square: 0, solution: 1},{row: 2, col: 1, value: 2, square: 0, solution: 1},{row: 2, col: 2, value: 3, square: 0, solution: 1},
+    {row: 2, col: 3, value: 4, square: 1, solution: 1},{row: 2, col: 4, value: 5, square: 1, solution: 1},{row: 2, col: 5, value: 6, square: 1, solution: 1},
+    {row: 2, col: 6, value: 7, square: 2, solution: 1},{row: 2, col: 7, value: 8, square: 2, solution: 1},{row: 2, col: 8, value: 9, square: 2, solution: 1}
   ],
   [
-    {row: 3, col: 0, value: 1, square: 3},{row: 3, col: 1, value: 2, square: 3},{row: 3, col: 2, value: 3, square: 3},
-    {row: 3, col: 3, value: 4, square: 4},{row: 3, col: 4, value: 5, square: 4},{row: 3, col: 5, value: 6, square: 4},
-    {row: 3, col: 6, value: 7, square: 5},{row: 3, col: 7, value: 8, square: 5},{row: 3, col: 8, value: 9, square: 5}
+    {row: 3, col: 0, value: 1, square: 3, solution: 1},{row: 3, col: 1, value: 2, square: 3, solution: 1},{row: 3, col: 2, value: 3, square: 3, solution: 1},
+    {row: 3, col: 3, value: 4, square: 4, solution: 1},{row: 3, col: 4, value: 5, square: 4, solution: 1},{row: 3, col: 5, value: 6, square: 4, solution: 1},
+    {row: 3, col: 6, value: 7, square: 5, solution: 1},{row: 3, col: 7, value: 8, square: 5, solution: 1},{row: 3, col: 8, value: 9, square: 5, solution: 1}
   ],
   [
-    {row: 4, col: 0, value: 1, square: 3},{row: 4, col: 1, value: 2, square: 3},{row: 4, col: 2, value: 3, square: 3},
-    {row: 4, col: 3, value: 4, square: 4},{row: 4, col: 4, value: 5, square: 4},{row: 4, col: 5, value: 6, square: 4},
-    {row: 4, col: 6, value: 7, square: 5},{row: 4, col: 7, value: 8, square: 5},{row: 4, col: 8, value: 9, square: 5}
+    {row: 4, col: 0, value: 1, square: 3, solution: 1},{row: 4, col: 1, value: 2, square: 3, solution: 1},{row: 4, col: 2, value: 3, square: 3, solution: 1},
+    {row: 4, col: 3, value: 4, square: 4, solution: 1},{row: 4, col: 4, value: 5, square: 4, solution: 1},{row: 4, col: 5, value: 6, square: 4, solution: 1},
+    {row: 4, col: 6, value: 7, square: 5, solution: 1},{row: 4, col: 7, value: 8, square: 5, solution: 1},{row: 4, col: 8, value: 9, square: 5, solution: 1}
   ],
   [
-    {row: 5, col: 0, value: 1, square: 3},{row: 5, col: 1, value: 2, square: 3},{row: 5, col: 2, value: 3, square: 3},
-    {row: 5, col: 3, value: 4, square: 4},{row: 5, col: 4, value: 5, square: 4},{row: 5, col: 5, value: 6, square: 4},
-    {row: 5, col: 6, value: 7, square: 5},{row: 5, col: 7, value: 8, square: 5},{row: 5, col: 8, value: 9, square: 5}
+    {row: 5, col: 0, value: 1, square: 3, solution: 1},{row: 5, col: 1, value: 2, square: 3, solution: 1},{row: 5, col: 2, value: 3, square: 3, solution: 1},
+    {row: 5, col: 3, value: 4, square: 4, solution: 1},{row: 5, col: 4, value: 5, square: 4, solution: 1},{row: 5, col: 5, value: 6, square: 4, solution: 1},
+    {row: 5, col: 6, value: 7, square: 5, solution: 1},{row: 5, col: 7, value: 8, square: 5, solution: 1},{row: 5, col: 8, value: 9, square: 5, solution: 1}
   ],
   [
-    {row: 6, col: 0, value: 1, square: 6},{row: 6, col: 1, value: 2, square: 6},{row: 6, col: 2, value: 3, square: 6},
-    {row: 6, col: 3, value: 4, square: 7},{row: 6, col: 4, value: 5, square: 7},{row: 6, col: 5, value: 6, square: 7},
-    {row: 6, col: 6, value: 7, square: 8},{row: 6, col: 7, value: 8, square: 8},{row: 6, col: 8, value: 9, square: 8}
+    {row: 6, col: 0, value: 1, square: 6, solution: 1},{row: 6, col: 1, value: 2, square: 6, solution: 1},{row: 6, col: 2, value: 3, square: 6, solution: 1},
+    {row: 6, col: 3, value: 4, square: 7, solution: 1},{row: 6, col: 4, value: 5, square: 7, solution: 1},{row: 6, col: 5, value: 6, square: 7, solution: 1},
+    {row: 6, col: 6, value: 7, square: 8, solution: 1},{row: 6, col: 7, value: 8, square: 8, solution: 1},{row: 6, col: 8, value: 9, square: 8, solution: 1}
   ],
   [
-    {row: 7, col: 0, value: 1, square: 6},{row: 7, col: 1, value: 2, square: 6},{row: 7, col: 2, value: 3, square: 6},
-    {row: 7, col: 3, value: 4, square: 7},{row: 7, col: 4, value: 5, square: 7},{row: 7, col: 5, value: 6, square: 7},
-    {row: 7, col: 6, value: 7, square: 8},{row: 7, col: 7, value: 8, square: 8},{row: 7, col: 8, value: 9, square: 8}
+    {row: 7, col: 0, value: 1, square: 6, solution: 1},{row: 7, col: 1, value: 2, square: 6, solution: 1},{row: 7, col: 2, value: 3, square: 6, solution: 1},
+    {row: 7, col: 3, value: 4, square: 7, solution: 1},{row: 7, col: 4, value: 5, square: 7, solution: 1},{row: 7, col: 5, value: 6, square: 7, solution: 1},
+    {row: 7, col: 6, value: 7, square: 8, solution: 1},{row: 7, col: 7, value: 8, square: 8, solution: 1},{row: 7, col: 8, value: 9, square: 8, solution: 1}
   ],
   [
-    {row: 8, col: 0, value: 1, square: 6},{row: 8, col: 1, value: 2, square: 6},{row: 8, col: 2, value: 3, square: 6},
-    {row: 8, col: 3, value: 4, square: 7},{row: 8, col: 4, value: 5, square: 7},{row: 8, col: 5, value: 6, square: 7},
-    {row: 8, col: 6, value: 7, square: 8},{row: 8, col: 7, value: 8, square: 8},{row: 8, col: 8, value: 9, square: 8}
+    {row: 8, col: 0, value: 1, square: 6, solution: 1},{row: 8, col: 1, value: 2, square: 6, solution: 1},{row: 8, col: 2, value: 3, square: 6, solution: 1},
+    {row: 8, col: 3, value: 4, square: 7, solution: 1},{row: 8, col: 4, value: 5, square: 7, solution: 1},{row: 8, col: 5, value: 6, square: 7, solution: 1},
+    {row: 8, col: 6, value: 7, square: 8, solution: 1},{row: 8, col: 7, value: 8, square: 8, solution: 1},{row: 8, col: 8, value: 9, square: 8, solution: 1}
   ]
 ];
 
 describe('generateBoard()', () => {
   it("generates a new board when calling generateBoard('easy', 'classic')", async () => {
     const result = await generateBoard('easy', 'classic');
-    // Should return new puzzle and solution for puzzle
-    expect(result.length).toBe(2);
     // Puzzle should have 9 row arrays
-    const newBoard = result[0];
+    const newBoard = result;
     expect(newBoard.length).toBe(9);
     // Rows in puzzle should have 9 elements
-    expect(newBoard[0].length).toBe(9)
+    expect(newBoard.length).toBe(9)
   });
 
 
