89e83aea8c26c773273d07927b25d5a1f9aa9911
mcken-vince <mcken.vince@gmail.com>
2022-01-10 11:27:52 -0700

    Add unit test for NumberSelection disabled prop

--- src/tests/NumberSelection.test.tsx
+++ src/tests/NumberSelection.test.tsx
@@ -3,7 +3,8 @@ import NumberSelection, {NumberSelectionProps} from '../components/NumberSelecti
 
 const renderNumberSelection = (props: Partial<NumberSelectionProps> = {}) => {
   const defaultProps = {
-    handleClick: jest.fn()
+    handleClick: jest.fn(),
+    disabled: false
   };
 
   return render(
@@ -14,5 +15,19 @@ const renderNumberSelection = (props: Partial<NumberSelectionProps> = {}) => {
 describe('<NumberSelection />', () => {
   it('renders without crashing', () => {
     renderNumberSelection();
+  });
+
+  it('disables all buttons if disabled prop is true', () => {
+    renderNumberSelection({disabled: true});
+    expect(screen.getByTestId('num-select-button-1')).toHaveClass('disabled');
+    expect(screen.getByTestId('num-select-button-2')).toHaveClass('disabled');
+    expect(screen.getByTestId('num-select-button-3')).toHaveClass('disabled');
+    expect(screen.getByTestId('num-select-button-4')).toHaveClass('disabled');
+    expect(screen.getByTestId('num-select-button-5')).toHaveClass('disabled');
+    expect(screen.getByTestId('num-select-button-6')).toHaveClass('disabled');
+    expect(screen.getByTestId('num-select-button-7')).toHaveClass('disabled');
+    expect(screen.getByTestId('num-select-button-8')).toHaveClass('disabled');
+    expect(screen.getByTestId('num-select-button-9')).toHaveClass('disabled');
+    expect(screen.getByTestId('num-select-button-clear')).toHaveClass('disabled');
   });
 });
