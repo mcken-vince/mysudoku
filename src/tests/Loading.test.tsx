a50b851e4ebe13e09db443226cc397e14f5dc547
mcken-vince <mcken.vince@gmail.com>
2022-01-10 11:16:13 -0700

    Remove unused import

--- src/tests/Loading.test.tsx
+++ src/tests/Loading.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Loading from '../components/Loading';
 
 const renderLoading = () => {
